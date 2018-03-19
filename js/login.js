08eecc7a7fdf2f5d13c54882f7a74365de488a2d
finen2 <timaro@live.se>
2018-03-19 20:40:13 +0100

    array and login work (some bugs is left)

--- js/login.js
+++ js/login.js
@@ -3,18 +3,81 @@ var login = () => {
     //login action
     var email = document.getElementById('email').value
     var password = document.getElementById('password').value
+    var message = "email or Password is wrong"
 
-    for(i= 0; i < users.length; i++)
-      if(email == users[i].email && password == users[i].password){
-        alert("Jippi");
-        console.log('you are logged in');
-      }else{
-        alert("wrong user/pass");
-        console.log('Error');
+    for(i= 0; i < users.length; i++){
+      if (email == users[i].email && password == users[i].password) {
+            window.location.replace(users[i].refPage);
+            return;
+          }
       }
+      //Displays wrong message if username/password is incorrect
+     document.querySelector('#wrongPass').innerHTML = message;
   };
 
- console.log(users);
+  document.getElementById("password")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+      if (event.keyCode == 13) {
+        document.getElementById("password").click(getInfo());
+      }
+    });
+
+
+ /*
+ // Checks witch page youre currently at and pops out the html
+
+var currentPage = window.location.pathname.split('/').pop(); // Split will split the path into different obijects
+
+// Accouts with acess to the page
+
+var objPeople = [{
+    username: "student",
+    password: "student",
+    account: "student",
+    refPage: (currentPage === 'index.html' || currentPage === '') ? 'html/student/courses.html' : 'student/courses.html',
+
+}, {
+    username: "teacher",
+    password: "teacher",
+    account: "teacher",
+    refPage: (currentPage === 'index.html' || currentPage === '') ? 'html/teacher/courses.html' : 'teacher/courses.html'
+}, {
+    username: "admin",
+    password: "admin",
+    account: "admin",
+    refPage: (currentPage === 'index.html' || currentPage === '') ? 'html/admin/adminStart.html' : 'admin/adminStart.html'
+}]
+
+
+
+// Puts typed username and password into variables
+
+function getInfo() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var message = "Du har skrivit in fel lösenord!";
+
+    //Loops accouts and checks for matches
+    for (i = 0; i < objPeople.length; i++) {
+        if (username == objPeople[i].username && password == objPeople[i].password) {
+            window.location.replace(objPeople[i].refPage);
+            return;
+        }
+    }
+
+    //Displays wrong message if username/password is incorrect
+    document.querySelector('#wrongPass').innerHTML = message;
+}
+
+document.getElementById("password")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        document.getElementById("password").click(getInfo());
+    }
+});
+ */
 
 //const squares = arr.map(x => x  * x);
 
--- js/loginData.js
+++ js/loginData.js
@@ -1,22 +1,27 @@
 // Arrays of login data.
+var currentPage = window.location.pathname.split('/').pop(); // Split will split the path into different obijects
 
 const users = [
-    {student:{
+    {
     firstName: 'Stina',
     lastName: 'Larsson',
     age: '14',
     email: 'st@kaka.se',
+    account:'student',
     password: 'kaka1234',
-    classes: 'street,balle,breackDance'
-    }},
-    {teacher:{
+    classes: 'street,balle,breackDance',
+    refPage: (currentPage === 'index.html' || currentPage === '') ? 'html/student.html' : 'student.html'
+    },
+    {
     firstName: 'Emmi',
     lastName: 'Rojas',
     age: '34',
     email: 'er@kaka.se',
+    account: 'teacher',
     password: 'kaka1234',
-    classes: 'street,balle,breackDance'
-  }}
+    classes: 'street,balle,breackDance',
+    refPage: (currentPage === 'index.html' || currentPage === '') ? 'html/teacher.html' : 'teacher.html'
+    }
 ];
 
 /*
